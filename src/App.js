e06cc6b3c8ebd96800836206b4f8634027719bc3
George Joubran <durd2001@gmail.com>
2021-11-24 12:01:38 +0200

    Added code for authentication

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import "./App.css";
 import React from "react";
 
-import { useNavigate } from "react-router";
-
 import Authentication from "./components/authentication/authentication.jsx";
 import TeamBuilder from "./components/teamBuilder/teamBuilder.jsx";
 import Authenticator from "./utils/Authentication";
+import { useNavigate } from "react-router";
 
 function App() {
   const [logIn, setLogIn] = React.useState(false);
   const navigate = useNavigate();
+
   React.useEffect(() => {
     if (Authenticator.isAccountLogin()) {
       navigate("teamBuilder");
